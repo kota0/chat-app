528d5e15ede9ef2bbd743ef198dd658481ed66e6
kota0 <kota.mac07@gmail.com>
2022-01-30 00:19:46 +0900

    投稿時刻の日本時間への修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,9 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @room = Room.find(params[:room_id])
+    @messages = @room.messages.includes(:user)
   end
+  
 
 
 
@@ -12,7 +14,7 @@ class MessagesController < ApplicationController
   if @message.save
     redirect_to room_messages_path(@room)
   else
-    render :index
+    @messages = @room.messages.includes(:user)
   end
  end
 
